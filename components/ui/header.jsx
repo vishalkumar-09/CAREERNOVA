579c9453a9163f5199ba22f4ed6f8121452b5044
Vishal Kumar Gaud <iamvishalkumar09@gmail.com>
2025-03-10 13:38:34 +0530

    Create Header component with Clerk Authentication (sign in/up)
    ➡️Add Clerk authentication (Sign In/Up)
    ➡️ Create Header component with auth links

---
+++ components/ui/header.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button"
+import { ChevronDown, FileText, GraduationCapIcon, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
+import { 
+    DropdownMenu, 
+    DropdownMenuTrigger, 
+    DropdownMenuContent,
+    DropdownMenuLabel, 
+    DropdownMenuSeparator,
+    DropdownMenuItem 
+  } from "@/components/ui/dropdown-menu";
+  
+import {SignedOut, SignedIn, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full border-black bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+        <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+            <Link href="/" >
+                <Image src="/logo.png" alt="CareerNova Logo" width={200} height={60}
+                className="h-12 py-1 w-auto object-contain"/>
+            </Link>
+            <div className="flex items-center space-x-2 md:space-x-4">
+                <SignedIn>
+                    <Link href="/dashboard">   
+                        <Button variant="outline">
+                            <LayoutDashboard className="h-4 w-4"/>
+                            <span className="hidden md:block">Industry Insights</span>
+                            
+                        </Button>
+                    </Link>
+                <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button className="bg-white text-black">
+                            <StarsIcon className="h-4 w-4"/>
+                            <span className="hidden md:block">Growth Tools</span>
+                            <ChevronDown className="h-4 w-4"/>
+                    </Button></DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-white text-black">
+                    <DropdownMenuItem>
+                        <Link href={'/resume'} className="flex items-center gap-2">
+                        <FileText className="h-4 w-4"/>
+                        <span>Build Resume</span>
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link href={'/ai-cover-letter'} className="flex items-center gap-2">
+                        <PenBox className="h-4 w-4"/>
+                        <span>Cover Letter</span>
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link href={'/interview'} className="flex items-center gap-2">
+                        <GraduationCapIcon className="h-4 w-4"/>
+                        <span>Interview Prep</span>
+                        </Link>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton>
+                        <Button variant="outline">Sign In</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton 
+                        appearance={{
+                            elements:{
+                                avatarBox:"w-10 h-10",
+                                UserButtonPopoverCard: "shadow-xl",
+                                userPreviewMainIdentifier: "front-semibold",
+                            },
+                        }}
+                        afterSignOutUrl="/"
+                    />
+                </SignedIn>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
